8830f08b13eefdf7702e393333f7be6a51e12416
Molly Singmaster <mollysingmaster@gmail.com>
2021-09-30 16:30:21 -0400

    added validation to the required input for users

--- index.js
+++ index.js
@@ -15,14 +15,24 @@
 // THEN I am taken to the corresponding section of the README
 
 
-var inquirer = require('inquirer');
-var fs = require('fs');
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 const questions = [
-    {
+      {
+        type: 'input',
+        message: 'Please provide the project title',
+        name: 'title',
+        // checked to make sure the user provided a value for the project title
+        validate: (value)=> {if (value) {return true} else {return 'Please enter a project title'}},
+        default: 'Project Title',
+      },
+      {
         type: 'input',
         message: 'Please provide a brief description of your project',
         name: 'Description',
+        validate: (value)=> {if (value) {return true} else {return 'Please enter a project description'}},
+        default: 'Project Description',
       },
       {
         type: 'input',
@@ -31,44 +41,56 @@ const questions = [
       },
       {
         type: 'input',
-        message: 'Instructions for installation',
+        message: 'How to install the application (optional)',
         name: 'Installation',
       },
       {
         type: 'input',
         message: 'Instructions for using the app',
+        validate: (value)=> {if (value) {return true} else {return 'Please enter instructions'}},
         name: 'Usage',
       },
       {
-        type: 'input',
-        message: 'License information',
+        type: 'list',
+        message: 'License information (optional)',
         name: 'License',
       },
       {
         type: 'input',
         message: 'Contributors',
+        validate: (value)=> {if (value) {return true} else {return 'Please enter contributors'}},
         name: 'Contributing',
       },
       {
         type: 'input',
-        message: 'Tests for application',
+        message: 'Tests for application (optional)',
         name: 'tests',
       },
       {
         type: 'input',
         message: 'Please enter Github username',
         name: 'Questions',
+        validate: (value)=> {if (value) {return true} else {return 'Please enter your username'}},
+        default: 'mollymoo002',
       },
       {
         type: 'input',
         message: 'Please enter your email address',
-        name: 'Questions',
+        validate: (value)=> {if (value) {return true} else {return 'Please enter your email address'}},
+        name: 'email',
       },
 ];
 inquirer.prompt(questions)
 
 
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(filename, data, err => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("Your README has been generated");
+    });
+}
 
 
 function init() {}
